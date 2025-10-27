a8968df17dec5964ec0ae3799b6e079967070384
Kamal Lamichhane <klamichhane@infinite.com>
2025-10-27 18:42:56 +0545

    commas removed from input string

--- Assignment1/third.py
+++ Assignment1/third.py
@@ -1,8 +1,10 @@
 # Input a sentence or a paragraph from user
 # and extract how many unique words are used and display the count
 
-para = input('Enter anything: ').replace(".", "").split(" ")
+para = input('Enter anything: ').replace(".", "").replace(",", "").split(" ")
 
 unique_words = set(para)
+
+print(unique_words)
 
 print("Number of unique words: ", len(unique_words))
