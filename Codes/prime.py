5eade180632a1527ebc3781ef0ef7e36356a9624
Kamal Lamichhane <klamichhane@infinite.com>
2025-10-28 13:36:06 +0545

    check prime number

--- Codes/prime.py
+++ Codes/prime.py
@@ -3,9 +3,11 @@ num = int(input("Enter a number: "))
 if num < 2:
     print(f'{num} is not a prime number')
 else:
+    flag = False
     for i in range(2, num):
         if num % i == 0:
             print(f'{num} is not a prime number')
+            flag = True
             break
-
-    print(f'{num} is a prime number')
+    if not flag:
+        print(f'{num} is a prime number')
